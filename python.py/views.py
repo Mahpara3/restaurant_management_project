0582471338a627c437a4a78f6ce6adb4786044bd
Sravani Mahpara Acharyulu <Mahparaacharyulu@gmail.com>
2025-08-19 01:49:39 +0530

    35183

--- python.py/views.py
+++ python.py/views.py
@@ -1,10 +1,17 @@
-from django.conf import settings
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.urls import path
+from . import views
 
-def homepages(request):
-    restaurant_name = "My Restaurant"
-    phone_number = settings.RESTAURANT_PHONE
-    return render(request, "home.html".{
-        "restaurant_name": restaurant_name,
-        "phone_number": phone_number
-    })
+@api_view(['GET'])
+def restaurant_menu(request):
+    menu = [
+        {"name": "Paneer Butter Masala", "description": "Rich and creamy panner curry", "price": 599.00},
+        {"name": "Chicken Biryani", "description": "Aromatic basmati rice with tender chicken", "price": 649.00},
+        {"name": "Margherita Pizza", "description": "Classic cheese pizza with tomato base", "price": 399.00},
+        {"name": "Masala Dosa", "description": "Crispy dosa stuffed with spiced potato and tomato filling", "price": 249.00},
+    ]
+
+urlpatterns = [
+    path('menu/', views.restaurant_menu, name='restaurant-menu'),
+]
