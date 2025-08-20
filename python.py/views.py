5b62187269e0426c169f7d93717e791863d31be4
Sravani Mahpara Acharyulu <Mahparaacharyulu@gmail.com>
2025-08-20 12:16:18 +0530

    35182

--- python.py/views.py
+++ python.py/views.py
@@ -1,17 +1,40 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.urls import path
-from . import views
+from django.db import models
+from .models import Restaurant
+from django.contrib import admin
+from .models import MenuItem, Order
 
-@api_view(['GET'])
-def restaurant_menu(request):
-    menu = [
-        {"name": "Paneer Butter Masala", "description": "Rich and creamy panner curry", "price": 599.00},
-        {"name": "Chicken Biryani", "description": "Aromatic basmati rice with tender chicken", "price": 649.00},
-        {"name": "Margherita Pizza", "description": "Classic cheese pizza with tomato base", "price": 399.00},
-        {"name": "Masala Dosa", "description": "Crispy dosa stuffed with spiced potato and tomato filling", "price": 249.00},
-    ]
 
-urlpatterns = [
-    path('menu/', views.restaurant_menu, name='restaurant-menu'),
-]
+class MenuItem(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="menu_items")
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.resturant.name})"
+
+class  Order(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="orders")
+    customer_name = models.CharField(max_length=100)
+    items = models.ManyToManyField(MenuItem)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer_name}"
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("name", "restaurant", "price", "is_available", "created_at")
+    list_filter = ("restaurant", "is_available")
+    search_fields = ("name", "description")
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=("id", "restaurant", "customer_name", "total_price", "created_at")
+    list_filter=("restaurant", "created_at")
+    search_fields=("customer_name")
+    filter_horizontal = ("items")
+
